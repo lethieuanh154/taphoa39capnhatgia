a4c39eb2e71033b4f49cf2942a8f783eedefd51f
Thieu Anh <lethieuanh154@gmail.com>
2025-05-12 12:26:10 +0700

    tesst

--- backend/Utility/get_env.py
+++ backend/Utility/get_env.py
@@ -1,15 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path=dotenv_path,override=True)
-else:
-    print(f".env file not found at: {dotenv_path}")
-
 
 UserName = os.getenv('UserName')
 LatestBranchId=os.getenv('LatestBranchId')
 Password=os.getenv('Password')
 retailer=os.getenv('retailer')
+
+print(UserName)
